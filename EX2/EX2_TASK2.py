cba809980ec59596905ec51ae7f41306a92be4d8
Jacques <jacquesdarosa@gmail.com>
2022-08-24 13:22:10 +0200

    change_4

---
+++ EX2/EX2_TASK2.py
@@ -0,0 +1,5 @@
+# task 2
+
+Text= 'Berlin is a world city of culture, politics, media and science.'
+
+print(Text[0],Text[-1])
---
+++ EX2/EX2_TASK3.py
@@ -0,0 +1,4 @@
+# task 3
+
+Text= 'Berlin is a world city of culture, politics, media and science.'
+print(Text[0:3].upper())
