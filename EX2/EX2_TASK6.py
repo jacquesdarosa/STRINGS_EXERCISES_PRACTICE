b5a93d874ab095ecb6b26dc9871dac94586a8981
Jacques <jacquesdarosa@gmail.com>
2022-08-24 13:50:27 +0200

    change_7

---
+++ EX2/EX2_TASK6.py
@@ -0,0 +1,10 @@
+# task 6
+
+text = '---Python programming---'
+disallowed = "-"
+for character in disallowed:
+  allowed_text = text.replace(character, "")
+print(allowed_text)
+
+# check careerkarma.com 
+
