d0a9e02385fe39fda7d2fa96c5abee45a62b1aec
Jacques <jacquesdarosa@gmail.com>
2022-08-24 12:47:38 +0200

    change_2

---
+++ Ex1_task3.py
@@ -0,0 +1,12 @@
+
+
+# task 3
+
+from re import I
+
+
+City = 'london'
+population = 9000000
+(text) = isinstance(City, (str))
+print("City", ":", City.capitalize(), '(', (str(text)), ')')
+print("Population", ":", population)
