6a043285d24c63d3175dbeedcd09d8cc387385e3
Jacques <jacquesdarosa@gmail.com>
2022-08-24 14:44:07 +0200

    change10

--- EX1/Ex1_task5.py
+++ EX1/Ex1_task5.py
@@ -1,10 +1,6 @@
 # task 5
 
-text = '''Berlin
-straddles the banks of the
-Spree, which flows into the
-Havel (a tributary of the Elbe)
-in the western borough of Spandau.'''
+text = '''Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau.'''
 
 print(text.split())
 
