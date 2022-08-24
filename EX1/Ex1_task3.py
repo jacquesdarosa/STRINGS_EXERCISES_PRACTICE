27eaca2bf69df211495dddb2c0ce8381271f01af
Jacques <jacquesdarosa@gmail.com>
2022-08-24 14:44:21 +0200

    change_11

--- EX1/Ex1_task3.py
+++ EX1/Ex1_task3.py
@@ -3,14 +3,14 @@
 # task 3
 
 
-'''City = 'london'
+City = 'london'
 population = 9000000
 (text) = isinstance(City, (str))
 print("City", ":", City.capitalize(), '(', (str(text)), ')')
-print("Population", ":", population)'''
+print("Population", ":", population)
 
 # another way to do this
-#task3
+# task3
 
 city, population = 'london', '9000000'
 print('City', ':', city.capitalize(), '(',(city.isalpha()),')')
