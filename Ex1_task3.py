fefe8ed1a0a7f7f5f2c4c4262fb91c42dc2450e2
Jacques <jacquesdarosa@gmail.com>
2022-08-24 12:48:31 +0200

    change_3

--- Ex1_task3.py
+++ Ex1_task3.py
@@ -5,8 +5,15 @@
 from re import I
 
 
-City = 'london'
+'''City = 'london'
 population = 9000000
 (text) = isinstance(City, (str))
 print("City", ":", City.capitalize(), '(', (str(text)), ')')
-print("Population", ":", population)
+print("Population", ":", population)'''
+
+# another way to do this
+#task3
+
+city, population = 'london', '9000000'
+print('City', ':', city.capitalize(), '(',(city.isalpha()),')')
+print('Population',':', population)
